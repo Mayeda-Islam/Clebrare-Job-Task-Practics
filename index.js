6f1eb187051c34e1ac4ced703c1a63ef1ff7099b
unknown <mayedakonika@gmail.com>
2023-03-26 02:51:33 +0600

    frame toggle added

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("frame").addEventListener("click", () => {
+  document.getElementById("frame").classList.toggle("pentagon");
+  console.log(document.getElementById("frame"));
+});
